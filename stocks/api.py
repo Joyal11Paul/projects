536ac6200dd62c2e3fd58489eafb00447ce2f470
Joyal <jpaul11@binghamton.edu>
2025-01-26 13:32:13 -0500

    updated

--- stocks/api.py
+++ stocks/api.py
@@ -1,5 +1,7 @@
-import finnhub, json
-finnhub_client = finnhub.Client(api_key="cualgahr01qof06ir99gcualgahr01qof06ir9a0")
+import finnhub, json, time, webbrowser
+import pandas as pd
+
+finnhub_client = finnhub.Client(api_key="cub7alhr01qsc2sl2eigcub7alhr01qsc2sl2ej0")
 
 def grab_info():
     data = finnhub_client.stock_symbols('US')
@@ -9,10 +11,36 @@ def grab_info():
 with open("info.json") as f:
     context = json.load(f)
 
-name = context[0]["displaySymbol"]
-print(name)
+info = []
 
-with open("individual_Stats.json", "w") as f:
-    data = finnhub_client.company_basic_financials(name, 'all')["metric"]
-    json.dump(data, f, indent=4)
+for stock in range(50):    
+    name = context[stock]["displaySymbol"]
+    data = finnhub_client.quote(name)
+    metrics = finnhub_client.company_basic_financials(name, 'all')["metric"]
     
+    try:
+        if metrics["52WeekLow"] * 3 < metrics["52WeekHigh"]:
+            info.append({
+                "Name": name,
+                "Current Price" : data['c'],
+                "Daily Low": data['h'],
+                "Daily High": data['l'],
+                "52 Week High": metrics["52WeekHigh"],
+                "52 Week Low": metrics["52WeekLow"],
+            })
+    except KeyError:
+        pass
+    
+    time.sleep(0.5)
+
+info = tuple(info)
+df = pd.DataFrame(info)
+
+print("\n", df, "\n")
+
+time.sleep(5)
+
+for tabs in info:
+    webbrowser.open(f"https://finance.yahoo.com/quote/{tabs["Name"]}/")
+    time.sleep(1)
+    break
